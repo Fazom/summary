73c6a75449242871f0d88a8a4a3095bd3caa5dbb
Fazom <kbelyavskiy2015@mail.ru>
2024-04-19 20:10:57 +0300

    my student projects

---
+++ Создание приложения на WPF(магазин одежды) с использованием базы данныз SQL и СУБД Postgresql/BD3LB/MainWindow.xaml.cs
@@ -0,0 +1,195 @@
+﻿using System.Data.Common;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Odbc;
+
+namespace BD3LB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        class User
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string password { get; set; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        List<User> users = new List<User>();
+        User selected_user = new User();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Driver={PostgreSQL ODBC Driver(ANSI)};Server=localhost;Database=streetwear;Uid=postgres;Pwd=yalokin4002";
+            OdbcConnection connection = new OdbcConnection(connectionString);
+            connection.Open();
+            OdbcCommand command = new OdbcCommand("SELECT id, name, password FROM users", connection);
+            OdbcDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                users.Add(new User {id = Convert.ToInt32(reader["id"]) , name = reader["name"].ToString(), password = reader["password"].ToString() });
+            }
+            connection.Close();
+
+            int login = 0;
+            int password = 0;
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (LogBlock.Text == users[i].name)
+                {
+                    if (PasBlock.Text == users[i].password)
+                    {
+                        selected_user = users[i];
+                        LogWindow.Visibility = Visibility.Hidden;
+                        ShopWindow.Visibility = Visibility.Visible;
+                        HelloText.Text = $"Здравствуйте, {selected_user.name}!";
+                        break;
+                    }
+                    else
+                    {
+                        password += 1;
+
+                    }
+                }
+                else if (LogBlock.Text != users[i].name)
+                {
+                    login += 1;
+
+                }
+            }
+            if (login >= users.Count)
+            {
+                MessageBox.Show("Неправильный пользователь");
+            }
+            else if (password >= users.Count)
+            {
+                MessageBox.Show("Неправильный пользователь");
+            }
+        }
+
+        private void BackLog_Click(object sender, RoutedEventArgs e)
+        {
+            ShopWindow.Visibility = Visibility.Hidden;
+            LogWindow.Visibility = Visibility.Visible;
+            
+        }
+
+        private void AddShirt_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Driver={PostgreSQL ODBC Driver(ANSI)};Server=localhost;Database=streetwear;Uid=postgres;Pwd=yalokin4002";
+            OdbcConnection connection = new OdbcConnection(connectionString);
+            connection.Open();
+            string queryString = $"INSERT INTO orders (product_id, user_id) VALUES (1, {selected_user.id})";
+
+            {
+                using (OdbcCommand command = new OdbcCommand(queryString, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            connection.Close();
+        }
+
+        private void AddShort_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Driver={PostgreSQL ODBC Driver(ANSI)};Server=localhost;Database=streetwear;Uid=postgres;Pwd=yalokin4002";
+            OdbcConnection connection = new OdbcConnection(connectionString);
+            connection.Open();
+            string queryString = $"INSERT INTO orders (product_id, user_id) VALUES (2, {selected_user.id})";
+
+            {
+                using (OdbcCommand command = new OdbcCommand(queryString, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            connection.Close();
+        }
+
+        private void AddKepka_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Driver={PostgreSQL ODBC Driver(ANSI)};Server=localhost;Database=streetwear;Uid=postgres;Pwd=yalokin4002";
+            OdbcConnection connection = new OdbcConnection(connectionString);
+            connection.Open();
+            string queryString = $"INSERT INTO orders (product_id, user_id) VALUES (3, {selected_user.id})";
+
+            {
+                using (OdbcCommand command = new OdbcCommand(queryString, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            connection.Close();
+        }
+
+        private void GoBasket_Click(object sender, RoutedEventArgs e)
+        {
+            ShopWindow.Visibility = Visibility.Hidden;
+            BasketWindow.Visibility = Visibility.Visible;
+
+            string connectionString = "Driver={PostgreSQL ODBC Driver(ANSI)};Server=localhost;Database=streetwear;Uid=postgres;Pwd=yalokin4002";
+            OdbcConnection connection = new OdbcConnection(connectionString);
+            connection.Open();
+            
+            OdbcCommand command = new OdbcCommand($"SELECT product.name, product.price FROM orders JOIN product ON orders.product_id = product.id WHERE orders.user_id = {selected_user.id};", connection);
+
+            OdbcDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                description.Text += reader["name"] + "   |   " + reader["price"] + "\n";
+            }
+            connection.Close();
+
+
+            //string connectionString = "Driver={PostgreSQL ODBC Driver(ANSI)};Server=localhost;Database=streetwear;Uid=postgres;Pwd=yalokin4002";
+            //OdbcConnection connection = new OdbcConnection(connectionString);
+            //connection.Open();
+            //OdbcCommand command = new OdbcCommand($"SELECT product.name, product.price FROM order JOIN product ON order.product_id = product.id WHERE order.user_id = {selected_user.id};", connection);
+            //OdbcDataReader reader = command.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    description.Text += reader["name"] + " " + reader["price"];
+            //    //users.Add(new User { id = Convert.ToInt32(reader["id"]), name = reader["name"].ToString(), password = reader["password"].ToString() });
+            //}
+            //connection.Close();
+        }
+
+        private void ByAll_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Driver={PostgreSQL ODBC Driver(ANSI)};Server=localhost;Database=streetwear;Uid=postgres;Pwd=yalokin4002;";
+            string sql = $"DELETE FROM orders WHERE user_id = {selected_user.id}"; // Замените на ваш SQL-запрос
+
+            using (OdbcConnection connection = new OdbcConnection(connectionString))
+            {
+                connection.Open();
+                using (OdbcCommand command = new OdbcCommand(sql, connection))
+                {
+                    int rowsAffected = command.ExecuteNonQuery();
+                    description.Text = "";
+                    MessageBox.Show($"Куплено {rowsAffected} товаров.");
+
+                }
+            }
+        }
+
+        private void BackToShop_Click(object sender, RoutedEventArgs e)
+        {
+            BasketWindow.Visibility = Visibility.Hidden;
+            ShopWindow.Visibility = Visibility.Visible;
+            
+        }
+    }
+}
---
+++ Создание простого задачника на WPF с использованием нереляционных баз данных/LB4ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace LB4ToDoList
+{
+    
+    
+
+
+    public partial class MainWindow : Window
+    {
+        private string filePath = "tasks.json";
+        private List<Task> tasks = new List<Task>();
+        private StackPanel stackPanel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            stackPanel = Base;
+            LoadTasks();
+        }
+        private void LoadTasks()
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                tasks = JsonConvert.DeserializeObject<List<Task>>(json);
+                UpdateTaskListView();
+            }
+        }
+        private void SaveTasks()
+        {
+            string json = JsonConvert.SerializeObject(tasks);
+            File.WriteAllText(filePath, json);
+        }
+
+        private void UpdateTaskListView()
+        {
+            Base.Children.Clear();
+            foreach (Task task in tasks)
+            {
+                Border border = new Border();
+                border.Margin = new Thickness(0, 40, 0, 0);
+                border.CornerRadius = new CornerRadius(30);
+                border.Background = Brushes.White;
+                border.Padding = new Thickness(0);
+                border.Height = 140;
+                border.Width = 300;
+                border.HorizontalAlignment = HorizontalAlignment.Center;
+
+                Grid grid = new Grid();
+
+                // Создание TextBlock для названия задачи
+                TextBlock titleTextBlock = new TextBlock();
+                titleTextBlock.Text = task.Title;
+                titleTextBlock.Width = 230;
+                titleTextBlock.Height = 25;
+                titleTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                titleTextBlock.VerticalAlignment = VerticalAlignment.Top;
+                titleTextBlock.Margin = new Thickness(0, 5, 0, 0);
+
+                // Создание TextBlock для описания задачи
+                TextBlock descriptionTextBlock = new TextBlock();
+                descriptionTextBlock.Text = task.Description;
+                descriptionTextBlock.TextWrapping = TextWrapping.Wrap;
+                descriptionTextBlock.Width = 230;
+                descriptionTextBlock.Height = 68;
+                descriptionTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                descriptionTextBlock.VerticalAlignment = VerticalAlignment.Top;
+                descriptionTextBlock.Margin = new Thickness(0, 30, 0, 0);
+
+                // Создание TextBlock для дедлайна задачи
+                TextBlock deadlineTextBlock = new TextBlock();
+                deadlineTextBlock.Text = task.DeadLine;
+                deadlineTextBlock.Width = 103;
+                deadlineTextBlock.Height = 37;
+                deadlineTextBlock.HorizontalAlignment = HorizontalAlignment.Left;
+                deadlineTextBlock.VerticalAlignment = VerticalAlignment.Bottom;
+                deadlineTextBlock.Margin = new Thickness(35, 0, 0, 0);
+
+                // Создание кнопки для выполнения задачи
+                Button addButton = new Button();
+                addButton.Content = "Выполнить";
+                addButton.HorizontalAlignment = HorizontalAlignment.Right;
+                addButton.VerticalAlignment = VerticalAlignment.Bottom;
+                addButton.Height = 40;
+                addButton.Width = 115;
+                addButton.Margin = new Thickness(0, 0, 27, 0);
+                addButton.Tag = task;
+                addButton.Click += Compleate;
+                addButton.Margin = new Thickness(0,0,30,5);
+
+                // Добавление элементов в Grid
+                grid.Children.Add(titleTextBlock);
+                grid.Children.Add(descriptionTextBlock);
+                grid.Children.Add(deadlineTextBlock);
+                grid.Children.Add(addButton);
+
+                // Установка Grid в качестве содержимого Border
+                border.Child = grid;
+                Base.Children.Add(border);
+            }
+
+        }
+
+       
+
+        class Task
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string DeadLine { get; set; }
+            public bool IsCompleted { get; set; }
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            BorderTask.Visibility = Visibility.Visible;
+        }
+        private void Compleate(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            Task taskToRemove = (Task)clickedButton.Tag;
+
+            // Удаление задачи из списка tasks
+            tasks.Remove(taskToRemove);
+            SaveTasks();
+            UpdateTaskListView();
+        }
+
+        private void AddTaskThis_Click(object sender, RoutedEventArgs e)
+        {
+            Task newTask = new Task
+            {
+                Id = tasks.Count + 1,
+                Title = NameT.Text,
+                Description = DiscT.Text,
+                DeadLine = DeadT.Text,
+                IsCompleted = false,
+            };
+            tasks.Add(newTask);
+            BorderTask.Visibility = Visibility.Hidden;
+            SaveTasks();
+            UpdateTaskListView();
+        }
+    }
+}
